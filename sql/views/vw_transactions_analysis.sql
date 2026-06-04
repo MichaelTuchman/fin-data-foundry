b0d2a155232fc149ef9b9253df991dc3101ddf94
Michael Tuchman <mftuchman@gmail.com>
2026-06-03 22:54:42 -0400

    updated and simplified transactions analysis downloadable

--- sql/views/vw_transactions_analysis.sql
+++ sql/views/vw_transactions_analysis.sql
@@ -1,70 +1,17 @@
--- this is the final cross-bank analy sis dataset
--- transaction_date
--- description
--- amount
--- account metadata
+create or replace view vw_transactions_analysis as
+select
+    source_system,
+    account_id,
+    row_id,
 
+    try_cast(transaction_date as date) as transaction_date,
+    description,
 
-CREATE OR REPLACE VIEW vw_transactions_analysis AS
+    try_cast(
+        regexp_replace(amount, '[,$]', '')
+        as decimal(18,2)
+    ) as amount,
 
-SELECT
-    cl.source_system,
-    cl.account_id,
+    check_number
 
-    a.account_label,
-    a.account_type,
-    a.institution,
-
-    cl.source_path,
-    cl.file_name,
-    cl.file_start_yyyymm,
-    cl.file_end_yyyymm,
-    cl.source_row_number,
-    cl.layout_id,
-
-    CAST(
-        date_parse(
-            MAX(
-                CASE
-                    WHEN cl.canonical_field = 'transaction_date'
-                    THEN cl.raw_value
-                END
-            ),
-            '%c/%e/%Y'
-        ) AS date
-    ) AS transaction_date,
-
-    MAX(
-        CASE
-            WHEN cl.canonical_field = 'description'
-            THEN cl.raw_value
-        END
-    ) AS description,
-
-    MAX(
-                CASE
-                    WHEN cl.canonical_field = 'amount'
-                    THEN cl.raw_value
-                END
-            )
-    ) AS amount
-
-FROM vw_transactions_canonical_long cl
-
-INNER JOIN metadata_accounts a
-  ON cl.source_system = a.source_system
- AND cl.account_id = a.account_id
-
-GROUP BY
-    cl.source_system,
-    cl.account_id,
-    a.account_label,
-    a.account_type,
-    a.institution,
-    cl.source_path,
-    cl.file_name,
-    cl.file_start_yyyymm,
-    cl.file_end_yyyymm,
-    cl.source_row_number,
-    cl.layout_id;
-
+from vw_transactions_wide;
