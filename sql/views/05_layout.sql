a6ed101399b2913ef36c3a7ae3a9da0c15f2e9c0
Michael Tuchman <mftuchman@gmail.com>
2026-06-06 21:45:35 -0400

    Extraction completed through step 5 before exploding rows

---
+++ sql/views/05_layout.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE VIEW layout_match AS
+SELECT
+    f.source_file_path,
+    f.source_file_name,
+    f.source_system,
+    f.account_id,
+    f.file_start_dt,
+    f.file_end_dt,
+    l.layout_id,
+    f.raw_row_serial,
+    f.col01,
+    f.col02,
+    f.col03,
+    f.col04,
+    f.col05,
+    f.col06,
+    f.col07,
+    f.col08,
+    f.col09,
+    f.col10
+FROM file_context f
+INNER JOIN metadata_layouts l
+    ON f.source_system = l.source_system
+   AND f.file_start_dt >= CAST(l.effective_start_dt AS date)
+   AND f.file_start_dt <= CAST(l.effective_end_dt AS date)
