eb2be09ae6105a0111673776ae47e92776432fde
Michael Tuchman <mftuchman@gmail.com>
2026-06-06 21:23:33 -0400

    01_raw.sql tested

--- sql/views/01_raw.sql
+++ sql/views/01_raw.sql
@@ -40,11 +40,11 @@ CREATE EXTERNAL TABLE IF NOT EXISTS raw_csv (
 ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
 WITH SERDEPROPERTIES (
     'separatorChar' = ',',
-    'quoteChar' = '"',
-    'escapeChar' = '\\'
+    'quoteChar' = '"'
 )
 STORED AS TEXTFILE
 LOCATION 's3://mftfinances/raw/'
 TBLPROPERTIES (
     'skip.header.line.count' = '0'
-)
+);
+
