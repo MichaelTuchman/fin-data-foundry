0b451cf3ac6187410468ebde814872a9cd28c456
Michael Tuchman <mftuchman@gmail.com>
2026-06-13 22:59:40 -0400

    file parsing

--- sql/views/04_context.sql
+++ sql/views/04_context.sql
@@ -1,38 +1,38 @@
-create or replace view finances.file_context as
-select
-  source_file_path,
-  regexp_extract(source_file_path, '[^/]+$') as source_file_name,
-  regexp_extract(source_file_path, 'raw/([^/]+)/', 1) as source_system,
-  regexp_extract(source_file_path, 'raw/[^/]+/([^/]+)/', 1) as account_id,
-  try_cast(
-    date_parse(
-      regexp_extract(source_file_path, '(\\d{6})', 1),
-      '%Y%m'
-    ) as date
-  ) as file_start_dt,
- date_add(
-    'day',
-    -1,
-    date_add(
-      'month',
-      1,
-      try_cast(
+CREATE OR REPLACE VIEW finances.file_context AS
+SELECT
+    source_file_path,
+    regexp_extract(source_file_path, '[^/]+$') AS source_file_name,
+    regexp_extract(source_file_path, 'raw/([^/]+)/', 1) AS source_system,
+    regexp_extract(source_file_path, 'raw/[^/]+/([^/]+)/', 1) AS account_id,
+    TRY_CAST(
         date_parse(
-          regexp_extract(source_file_path, '(\\d{6})', 1),
-          '%Y%m'
-        ) as date
-      )
-    )
-  ) as file_end_dt,
-  derived_row_serial,
-  col01,
-  col02,
-  col03,
-  col04,
-  col05,
-  col06,
-  col07,
-  col08,
-  col09,
-  col10
-from finances.raw_serial
+            regexp_extract(source_file_path, '(\d{6})', 1),
+            '%Y%m'
+        ) AS date
+    ) AS file_start_dt,
+    date_add(
+        'day',
+        -1,
+        date_add(
+            'month',
+            1,
+            TRY_CAST(
+                date_parse(
+                    regexp_extract(source_file_path, '(\d{6})', 1),
+                    '%Y%m'
+                ) AS date
+            )
+        )
+    ) AS file_end_dt,
+    derived_row_serial,
+    col01,
+    col02,
+    col03,
+    col04,
+    col05,
+    col06,
+    col07,
+    col08,
+    col09,
+    col10
+FROM finances.raw_serial
