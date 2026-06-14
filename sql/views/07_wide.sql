90165f801784b3c3fd658644b505f1dc2695103f
Michael Tuchman <mftuchman@gmail.com>
2026-06-14 18:31:51 -0400

    parsing money issues

--- sql/views/07_wide.sql
+++ sql/views/07_wide.sql
@@ -9,20 +9,42 @@ SELECT
     layout_id,
     derived_row_serial,
 
-    MAX(CASE WHEN lower(canonical_field) = 'transaction_date'
-        THEN raw_value END) AS transaction_date,
+    MAX(CASE
+        WHEN lower(canonical_field) = 'transaction_date'
+        THEN raw_value
+    END) AS transaction_date,
 
-    MAX(CASE WHEN lower(canonical_field) = 'description'
-        THEN raw_value END) AS description,
+    MAX(CASE
+        WHEN lower(canonical_field) = 'description'
+        THEN raw_value
+    END) AS description,
 
-    MAX(CASE WHEN lower(canonical_field) = 'amount'
-        THEN raw_value END) AS amount,
+    MAX(CASE
+        WHEN lower(canonical_field) = 'amount'
+        THEN raw_value
+    END) AS amount,
 
-    MAX(CASE WHEN lower(canonical_field) = 'check_number'
-        THEN raw_value END) AS check_number,
+    MAX(CASE
+        WHEN lower(canonical_field) = 'amount'
+        THEN TRY_CAST(
+            regexp_replace(
+                regexp_replace(raw_value, '[$,]', ''),
+                '^[(](.*)[)]$',
+                '-$1'
+            )
+            AS decimal(18,2)
+        )
+    END) AS amount_d,
 
-    MAX(CASE WHEN lower(canonical_field) = 'status'
-        THEN raw_value END) AS status
+    MAX(CASE
+        WHEN lower(canonical_field) = 'check_number'
+        THEN raw_value
+    END) AS check_number,
+
+    MAX(CASE
+        WHEN lower(canonical_field) = 'status'
+        THEN raw_value
+    END) AS status
 
 FROM finances.canon_long
 WHERE validation_passed
@@ -35,4 +57,3 @@ GROUP BY
     file_end_dt,
     layout_id,
     derived_row_serial
-
