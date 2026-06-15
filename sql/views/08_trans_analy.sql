5d348d5c151b5c6b1f7d47086f4a102034307449
Michael Tuchman <mftuchman@gmail.com>
2026-06-14 21:05:23 -0400

    Update 08_trans_analy.sql to fix blank dates

--- sql/views/08_trans_analy.sql
+++ sql/views/08_trans_analy.sql
@@ -12,13 +12,16 @@ SELECT
     cw.layout_id,
     cw.derived_row_serial,
     cw.transaction_date,
-    TRY_CAST(cw.transaction_date AS date) AS transaction_dt,
+    COALESCE(
+        TRY_CAST(cw.transaction_date AS date),
+        TRY(date_parse(cw.transaction_date, '%m/%d/%Y'))
+    ) AS transaction_dt,
     cw.description,
     cw.amount,
-    TRY_CAST(cw.amount AS decimal(18,2)) AS amount_d,
+    cw.amount_d,
     cw.check_number,
     cw.status
 FROM finances.canon_wide cw
 LEFT JOIN finances.metadata_accounts ma
     ON cw.source_system = ma.source_system
-   AND cw.account_id = ma.account_id
+   AND cw.account_id = ma.account_id;
