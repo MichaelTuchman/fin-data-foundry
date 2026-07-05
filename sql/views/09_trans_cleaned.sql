72d7b9f7d6be9f4af90ab70791ef078bb7fe79b0
Michael Tuchman <mftuchman@gmail.com>
2026-07-04 20:46:34 -0400

    Fixed minor punctuation rule

--- sql/views/09_trans_cleaned.sql
+++ sql/views/09_trans_cleaned.sql
@@ -12,6 +12,20 @@ SELECT
     layout_id,
     derived_row_serial,
     transaction_date,
+CREATE OR REPLACE VIEW trans_cleaned AS
+SELECT
+    source_file_path,
+    source_file_name,
+    source_system,
+    account_id,
+    account_label,
+    account_type,
+    institution,
+    file_start_dt,
+    file_end_dt,
+    layout_id,
+    derived_row_serial,
+    transaction_date,
     transaction_dt,
     description AS source_description,
     trim(
@@ -19,14 +33,22 @@ SELECT
             regexp_replace(
                 regexp_replace(
                     regexp_replace(
-                        regexp_replace(description, '[\r\n\t]+', ' '),
-                        '\bPURCHASE AUTHORIZED ON +[0-9]{1,2}/[0-9]{1,2} +',
+                        regexp_replace(
+                            regexp_replace(
+                                description,
+                                '[\r\n\t]+',
+                                ' '
+                            ),
+                            '\bPURCHASE AUTHORIZED ON +[0-9]{1,2}/[0-9]{1,2} +',
+                            ''
+                        ),
+                        '\bRECURRING PAYMENT AUTHORIZED ON +[0-9]{1,2}/[0-9]{1,2} +',
                         ''
                     ),
-                    '\bRECURRING PAYMENT AUTHORIZED ON +[0-9]{1,2}/[0-9]{1,2} +',
-                    ''
+                    '\b(LLC|INC|CORP|LTD|LP|LLP|PLLC|PC)([A-Z])',
+                    '$1 $2'
                 ),
-                '\b(LLC|INC|CORP|LTD|LP|LLP|PLLC|PC)([A-Z])',
+                '([!?,.;:])([A-Za-z])',
                 '$1 $2'
             ),
             ' {2,}',
@@ -38,3 +60,4 @@ SELECT
     check_number,
     status
 FROM trans_analy;
+
