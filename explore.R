475b6464d42673935f93eabd8f35db766544ea22
Michael Tuchman <mftuchman@gmail.com>
2026-07-09 23:32:44 -0400

    added views to be built directly from RStudio

--- explore.R
+++ explore.R
@@ -6,6 +6,34 @@ library(knitr)
 library(scales)
 
 options(rstudio.connectionObserver.errorsSuppressed = TRUE)
+# Run athena view helper ----
+run_athena_view <- function(file_name, conn) {
+  if (!file.exists(file_name)) {
+    stop("SQL file not found: ", file_name)
+  }
+  
+  sql <- paste(
+    readLines(file_name, warn = FALSE, encoding = "UTF-8"),
+    collapse = "\n"
+  )
+  
+  sql <- sub("^\ufeff", "", sql)
+  
+  if (!grepl(
+    "^\\s*CREATE\\s+OR\\s+REPLACE\\s+VIEW\\b",
+    sql,
+    ignore.case = TRUE
+  )) {
+    stop("The SQL file must begin with CREATE OR REPLACE VIEW.")
+  }
+  
+  DBI::dbExecute(conn, sql)
+  
+  message("Successfully executed: ", file_name)
+  
+  invisible(TRUE)
+}
+# end of helper functions ----
 
 py_require(c("boto3", "numpy"))
 
